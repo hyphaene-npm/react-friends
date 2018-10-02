6399f8ff7e92edb244f245248d5476ac19c6d678
Maximilien Garenne <maximilien@whatson-web.com>
2018-10-02 12:00:28 +0200

    [react-friends] clean

--- src/recomposeHelpers.js
+++ src/recomposeHelpers.js
@@ -1,9 +1,10 @@
 import classnames from 'classnames';
 import { withState, withHandlers } from 'recompose';
-
+// ========================= withState
 export const withStateActiveTab = defaultTab => withState('activeTab', 'setActiveTab', defaultTab);
 export const withStateOpen = withState('isOpen', 'setIsOpen', false);
 export const withData = withState('data', 'setData', null);
+// ========================= unit handlers
 
 export const styleTabHandler = props => tabId => classnames({ active: props.activeTab === tabId });
 export const toggleHandler = props => tab => () => {
@@ -12,6 +13,7 @@ export const toggleHandler = props => tab => () => {
 	}
 };
 
+// ========================= composed handlers
 export const withToggle = withHandlers({
 	onToggle: toggleHandler,
 	onStyleTab: styleTabHandler,
